b3b718f6801672f2df92c76ec12c3ea162967cf8
stevenvelezg <steven.velezgarcia@gmail.com>
2022-01-17 18:15:57 -0500

    water container #11

---
+++ mostwatercontainer.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var maxArea = function(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+    
+    while (left < right) {
+        const area = Math.min(height[left], height[right]) * (right - left);
+        if (area > max) {
+            max = area;
+        }
+        if (height[left] < height[right]){
+            left ++;
+        }
+        else {
+            right--;
+        }
+    }
+    return max;
+};
